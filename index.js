cad47255659c495fe42134975c5b3c33acbd4641
AndreSokol <sokolov-as-i@yandex.ru>
2014-12-17 20:02:59 +0300

    renewed dependencies, working on db

--- index.js
+++ index.js
@@ -7,6 +7,22 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views')
 app.use(express.static(__dirname + '/public'));
 
+//-----  connect database  ---------
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.send(result.rows); }
+    });
+  });
+});
+//------------------------------------
+
 app.get('/', pctrl.home);
 app.get('/admin', pctrl.admin);
 app.get('*', function (request, responce) {
