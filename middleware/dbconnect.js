e04be9ab16669ef82fea2fe45d842c6acde976ac
AndreSokol <sokolov-as-i@yandex.ru>
2014-12-21 23:39:03 +0300

    get_data method+ /db/%table_name% route featured

--- middleware/dbconnect.js
+++ middleware/dbconnect.js
@@ -1,8 +1,7 @@
 var pg = require('pg'),
 	db_url = process.env.DATABASE_URL || "postgres://postgres:lollipop11@localhost:5432/postgres";
 
-exports.get_data = function (qstring) {
-	var ans = '';
+exports.get_data = function (qstring, callback) {
 	pg.connect(db_url, function(err, client, done) {
 		/*var handleError = function(err) {
 			if(!err) return false;
@@ -14,15 +13,17 @@ exports.get_data = function (qstring) {
 		
 		console.log(qstring);
 		
-		var query = client.query(qstring);
+		var query = client.query(qstring),
+			cnt = 0;
 		
-		query.on('row', function(row) {
-			console.log(row.name);
-			ans += row.name;
-			console.log("ans - " + ans);
+		query.on('row', function (row, result) {
+			result.addRow(row);
+		});
+		
+		query.on('end', function(result) {
+			console.log("Got " + result.rows.length + " rows when asked " + qstring);
+			callback(result);
 		});
 		done();
 	});
-	console.log(ans);
-	return ans;
 };
--- route/dbcheck.js
+++ route/dbcheck.js
@@ -3,7 +3,7 @@ var db = require('../middleware/dbconnect');
 exports.main = function (req, res) {
 	var qstring = req.url;
 	qstring = qstring.slice(4, db.length);
-	var ans = db.get_data("SELECT * FROM " + qstring);
-	console.log('db check - ' + ans);
-	res.send();
+	db.get_data("SELECT * FROM " + qstring, function (result) {
+		res.send(result.rows);
+	});
 };
