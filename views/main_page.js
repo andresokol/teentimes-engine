89ae8001091b0399edcb7f0ecb6d3b4b00bbb6e4
AndreSokol <sokolov-as-i@yandex.ru>
2014-12-23 16:36:25 +0300

    Template update

--- views/main_page.js
+++ views/main_page.js
@@ -9,7 +9,7 @@ exports.main = function(req, res, posts_on_page) {
 		console.log("Executin callback...");
 		console.log("Posts display count: " + arr.length);
 		for (var i = 0; i < posts_on_page && i < arr.length; i++) {
-			ans += '<div class="article"><h3><a href="/articles/001">' + arr[i].title + '</a></h3><div id="imgcap"><img src="http://41.media.tumblr.com/8f192e1322f39d50211003582dfc4433/tumblr_nct7uv7zVm1qapi0oo1_400.jpg" /><p>Bing!</p></div>' + arr[i].body + '<div id="time"><time>' + arr[i].time + '</time></div></div>';
+			ans += '<div class="article"><h3><a href="/articles/001">' + arr[i].title + '</a></h3>' + arr[i].body + '<div id="time"><time>' + arr[i].time + '</time></div></div>';
 		};
 		console.log("Printin' answer " + ans);
 		res.render('../templates/main', {
