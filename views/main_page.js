c9f0830bed0bdf3b16dff36cb04e8ee26ba77f9e
AndreSokol <sokolov-as-i@yandex.ru>
2014-12-25 08:50:13 +0300

    Fixed time, remove some debug

--- views/main_page.js
+++ views/main_page.js
@@ -2,16 +2,12 @@ var db = require('../middleware/dbconnect'),
 	table = "test_posts";
 
 exports.main = function(req, res, posts_on_page) {
-	console.log("Gettin' page...");
 	db.get_data(table, function (query) {
 		var ans = '',
 			arr = query.rows;
-		console.log("Executin callback...");
-		console.log("Posts display count: " + arr.length);
 		for (var i = 0; i < posts_on_page && i < arr.length; i++) {
-			ans += '<div class="article"><h3><a href="/articles/001">' + arr[i].title + '</a></h3>' + arr[i].body + '<div id="time"><time>' + arr[i].time + '</time></div></div>';
+			ans += '<div class="article"><h3><a href="/articles/001">' + arr[i].title + '</a></h3>' + arr[i].body + '<div id="time"><time>' + String(arr[i].created).slice(16, 21) + '</time></div></div>';
 		};
-		console.log("Printin' answer " + ans);
 		res.render('../templates/main', {
 			body: ans
 		});
