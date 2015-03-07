d756cbbb4a94dbba2c951ed616601c29d4c13e58
AndreSokol <sokolov-as-i@yandex.ru>
2015-03-07 17:08:52 +0300

    Translated UI to russian

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,43 +1,43 @@
 $(document).ready( function () {
 	$('#all-btn').click(function () {
 		$('.container').children().addClass('hddn');
-		$('button').removeClass('btn-primary');
-		$(this).addClass('btn-primary');
+		$('button').removeClass('btn-info');
+		$(this).addClass('btn-info');
 		$('#all').removeClass('hddn');
 	});
 	
 	$('#articles-btn').click(function () {
 		$('.container').children().addClass('hddn');
-		$('button').removeClass('btn-primary');
-		$(this).addClass('btn-primary');
+		$('button').removeClass('btn-info');
+		$(this).addClass('btn-info');
 		$('.articles').removeClass('hddn');
 	});
 	
 	$('#img-btn').click(function () {
 		$('.container').children().addClass('hddn');
-		$('button').removeClass('btn-primary');
-		$(this).addClass('btn-primary');
+		$('button').removeClass('btn-info');
+		$(this).addClass('btn-info');
 		$('.img').removeClass('hddn');
 	});
 	
 	$('#music-btn').click(function () {
 		$('.container').children().addClass('hddn');
-		$('button').removeClass('btn-primary');
-		$(this).addClass('btn-primary');
+		$('button').removeClass('btn-info');
+		$(this).addClass('btn-info');
 		$('.music').removeClass('hddn');
 	});
 	
 	$('#lastissue-btn').click(function () {
 		$('.container').children().addClass('hddn');
-		$('button').removeClass('btn-primary');
-		$(this).addClass('btn-primary');
+		$('button').removeClass('btn-info');
+		$(this).addClass('btn-info');
 		$('.lastissue').removeClass('hddn');
 	});
 	
 	$('#literature-btn').click(function () {
 		$('.container').children().addClass('hddn');
-		$('button').removeClass('btn-primary');
-		$(this).addClass('btn-primary');
+		$('button').removeClass('btn-info');
+		$(this).addClass('btn-info');
 		$('.literature').removeClass('hddn');
 	});
 });
