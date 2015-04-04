d52c5e6eccad34c1baa2f2240288f449e03c01fb
AndreSokol <sokolov-as-i@yandex.ru>
2015-04-04 13:41:20 +0300

    still fixin

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express = require('express'),
     app = express(),
     middleware = require('./middleware')(app, express);
 
+console.log(app.get('port'));
+
 app.listen(app.get('port'), function() {
 	console.log("Node app is running at localhost:" + app.get('port'));
 });
